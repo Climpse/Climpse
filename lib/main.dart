0e69a0e630a5e89b622afbc27c3082c40c9a86f6
Climpse <climpse.project@outlook.com>
2023-03-06 23:50:40 -0300

    criando telas de cadastro do usuario e veíuclo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:climpse/pages/home/home_page.dart';
 import 'package:climpse/pages/initial/initial_page.dart';
 import 'package:climpse/pages/login/login_page.dart';
-import 'package:climpse/pages/register/register_page.dart';
+import 'package:climpse/pages/register/user/register_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
---
+++ lib/pages/register/user/register_page.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController cpfController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController genreController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                    width: 200,
+                    height: 150,
+                    child: TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/");
+                        },
+                        child: Text("Voltar"))),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Digite seu nome:',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: cpfController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Digite seu CPF:',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Digite seu email:',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Digite sua senha:',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: genreController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Sexo:',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: phoneNumberController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Número de celular:',
+                ),
+              ),
+            ),
+            Container(
+              child: TextButton(
+                  onPressed: () {
+                    // return cpfController.text
+                    Navigator.pushNamed(context, "/home");
+                  },
+                  child: Text("Criar Conta")),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
