05fce896acd990baa899eeb6f62d31f490a801f1
nathan.berger <nathan.berger@cwi.com.br>
2023-03-06 22:57:43 -0300

    feat: estruturando tela home

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -8,40 +8,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.fromLTRB(50, 300, 0, 0),
-            child: const Text(
-              "CLIMPSE.",
-              textAlign: TextAlign.center,
-              textScaleFactor: 4.0,
-              style: TextStyle(
-                color: Colors.black87,
-                fontWeight: FontWeight.w900,
-                fontSize: 18.0,
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Text("Home Page"),
             ),
-          ),
-          Container(
-            width: 200,
-            child: OutlinedButton(
-              child: const Text('Login'),
-              style: TextButton.styleFrom(foregroundColor: Colors.black),
-              onPressed: () {
-                Navigator.pushNamed(context, "/login");
-              },
+            Container(
+              child: TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/");
+                  },
+                  child: Text("Voltar")),
             ),
-          ),
-          Container(
-            width: 200,
-            child: OutlinedButton(
-              child: const Text('Cadastre-se'),
-              style: TextButton.styleFrom(foregroundColor: Colors.black),
-              onPressed: () {},
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
