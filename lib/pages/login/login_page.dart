5394badd0f51342aa471c858b954525c36ff6946
nathan.berger <nathan.berger@cwi.com.br>
2023-03-06 22:45:58 -0300

    feat: criando tela de login

---
+++ lib/pages/login/login_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  TextEditingController cpfController = TextEditingController();
+
+  void enviarDados(dados) {
+    print(dados);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                    width: 200,
+                    height: 150,
+                    child: TextButton(
+                        onPressed: () {
+                          // return cpfController.text
+                          Navigator.pushNamed(context, "/");
+                        },
+                        child: Text("Voltar"))),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: cpfController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'CPF',
+                ),
+              ),
+            ),
+            Container(
+              child: TextButton(
+                  onPressed: () {
+                    // return cpfController.text
+                    Navigator.pushNamed(context, "/home");
+                  },
+                  child: Text("Logar")),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
