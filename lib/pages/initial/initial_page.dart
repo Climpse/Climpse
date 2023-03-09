8e61ae2e9df6e2be36ec5742558ec40878b49122
Climpse-project <126828758+Climpse@users.noreply.github.com>
2023-03-09 20:04:21 -0300

    style: estilizando botões da tela inicial

--- lib/pages/initial/initial_page.dart
+++ lib/pages/initial/initial_page.dart
@@ -1,49 +1,60 @@
 import 'package:flutter/material.dart';
 
 class InitialPage extends StatefulWidget {
+  const InitialPage({super.key});
+
   @override
   _InitialPageState createState() => _InitialPageState();
 }
 
 class _InitialPageState extends State<InitialPage> {
+  @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.fromLTRB(50, 300, 0, 0),
-            child: const Text(
-              "CLIMPSE.",
-              textAlign: TextAlign.center,
-              textScaleFactor: 4.0,
-              style: TextStyle(
-                color: Colors.black87,
-                fontWeight: FontWeight.w900,
-                fontSize: 18.0,
+      body: Center(
+        widthFactor: width,
+        heightFactor: height,
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 200.0),
+              child: const Text(
+                "CLIMPSE.",
+                textAlign: TextAlign.center,
+                textScaleFactor: 4.0,
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontWeight: FontWeight.w900,
+                  fontSize: 18.0,
+                ),
               ),
             ),
-          ),
-          Container(
-            width: 200,
-            child: OutlinedButton(
-              child: const Text('Login'),
-              style: TextButton.styleFrom(foregroundColor: Colors.black),
-              onPressed: () {
-                Navigator.pushNamed(context, "/login");
-              },
+            Container(
+              padding: const EdgeInsets.only(top: 90.0),
+              width: 200,
+              child: OutlinedButton(
+                style: TextButton.styleFrom(foregroundColor: Colors.black),
+                onPressed: () {
+                  Navigator.pushNamed(context, "/login");
+                },
+                child: const Text('Login'),
+              ),
             ),
-          ),
-          Container(
-            width: 200,
-            child: OutlinedButton(
-              child: const Text('Cadastre-se'),
-              style: TextButton.styleFrom(foregroundColor: Colors.black),
-              onPressed: () {
-                Navigator.pushNamed(context, "/register");
-              },
+            SizedBox(
+              width: 200,
+              child: OutlinedButton(
+                style: TextButton.styleFrom(foregroundColor: Colors.black),
+                onPressed: () {
+                  Navigator.pushNamed(context, "/register");
+                },
+                child: const Text('Cadastre-se'),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
