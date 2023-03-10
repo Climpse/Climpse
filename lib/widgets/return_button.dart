6ae041ac7535e7bfe0f20d856432389b9306ed03
Climpse-project <126828758+Climpse@users.noreply.github.com>
2023-03-09 21:21:34 -0300

    feat: adicionando props de texto

--- lib/widgets/return_button.dart
+++ lib/widgets/return_button.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
 
 class ReturnButton extends StatelessWidget {
-  const ReturnButton({super.key});
+  final String message;
+  const ReturnButton(this.message, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: const [
-        Padding(
-          padding: EdgeInsets.only(left: 7),
-        ),
-        Image(
-          width: 20,
-          height: 20,
-          image: NetworkImage(
-              "https://www.iconpacks.net/icons/2/free-arrow-left-icon-3099-thumb.png"),
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 10),
-        ),
-        Text("Voltar"),
-      ],
+    return InkWell(
+      onTap: () {
+        Navigator.pop(context);
+      },
+      child: Row(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 7),
+          ),
+          const Image(
+            width: 20,
+            height: 20,
+            image: NetworkImage(
+                "https://www.iconpacks.net/icons/2/free-arrow-left-icon-3099-thumb.png"),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 10),
+          ),
+          Text(message),
+        ],
+      ),
     );
   }
 }
