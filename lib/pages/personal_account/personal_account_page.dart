c10b8e0d323f2e774a8d20c94772649015015460
Climpse <climpse.project@outlook.com>
2023-03-11 01:15:03 -0300

    feat: finalizando tela de dados pessoais

--- lib/pages/personal_account/personal_account_page.dart
+++ lib/pages/personal_account/personal_account_page.dart
@@ -1,60 +1,81 @@
+import 'package:climpse/globals/global_variables.dart' as globals;
+import 'package:climpse/hooks/use_user.hook.dart';
+import 'package:climpse/model/user.model.dart';
 import 'package:flutter/material.dart';
+import 'package:climpse/constants/images_url.dart' as images;
 
-class PersonalAccountPage extends StatelessWidget {
+class PersonalAccountPage extends StatefulWidget {
   const PersonalAccountPage({super.key});
 
-  //TODO VERIFICAÇÃO DE SEXO PARA SETAR AVATAR RESPECTIVO
+  @override
+  State<PersonalAccountPage> createState() => _PersonalAccountPageState();
+}
+
+class _PersonalAccountPageState extends State<PersonalAccountPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Expanded(
-          child: ListView(
-            children: <Widget>[
-              Container(
-                child: Image.network(
-                    'https://cdn-icons-png.flaticon.com/512/147/147144.png'),
-              ),
-              ListView(
-                shrinkWrap: true,
-                children: const <Widget>[
-                  ListTile(
-                    leading: Icon(Icons.person),
-                    title: Text('Nome'),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.lock),
-                    title: Text('CPF'),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.email),
-                    title: Text('Email'),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.password),
-                    title: Text('Senha'),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.person_outlined),
-                    title: Text('Sexo'),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.phone),
-                    title: Text('Celular'),
-                  ),
-                ],
-              ),
-              IconButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/edit/user');
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                Image.network(images.femaleAvatar),
+                FutureBuilder<User>(
+                  future: getUserDetails(id: globals.globalUserId),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasData) {
+                      User userData = snapshot.data!;
+                      return ListView(
+                        shrinkWrap: true,
+                        children: <Widget>[
+                          ListTile(
+                            leading: const Icon(Icons.person),
+                            title: Text(userData.nome.toString()),
+                          ),
+                          ListTile(
+                            leading: const Icon(Icons.lock),
+                            title: Text(userData.cpf.toString()),
+                          ),
+                          ListTile(
+                            leading: const Icon(Icons.email),
+                            title: Text(userData.email.toString()),
+                          ),
+                          ListTile(
+                            leading: const Icon(Icons.password),
+                            title: Text(userData.senha.toString()),
+                          ),
+                          ListTile(
+                            leading: const Icon(Icons.person_outlined),
+                            title: Text(userData.sexo.toString()),
+                          ),
+                          ListTile(
+                            leading: const Icon(Icons.phone),
+                            title: Text(userData.celular.toString()),
+                          ),
+                        ],
+                      );
+                    }
+                    return const Text('Carregando informações');
                   },
-                  icon: const Icon(Icons.edit)),
-            ],
+                ),
+                IconButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/edit/user');
+                    },
+                    icon: const Icon(Icons.edit)),
+              ],
+            ),
           ),
-        ),
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
