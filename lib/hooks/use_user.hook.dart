031ebe980b0a1346e0daf646e0ae07dc47e9d526
Climpse <climpse.project@outlook.com>
2023-03-11 14:51:34 -0300

    feat: validação de login

--- lib/hooks/use_user.hook.dart
+++ lib/hooks/use_user.hook.dart
@@ -1,16 +1,21 @@
 import 'package:climpse/constants/base_url.dart';
 import 'package:climpse/model/user.model.dart';
+import 'package:climpse/widgets/show_custom_dialog.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:climpse/globals/global_variables.dart' as globals;
 
 final dio = Dio();
 
-login({cpf}) async {
+login({context, cpf}) async {
   try {
     final response =
         (await dio.get('${BaseUrl.baseUrl}/usuarios/buscar/$cpf')).data;
-    return response['id'];
-  } catch (e) {
-    return 'error';
+
+    globals.userId = response['id'];
+    Navigator.pushNamed(context, "/home");
+  } on DioError catch (error) {
+    showCustomDialog(context, 'Ocorreu um erro', 'Você não está cadastrado');
   }
 }
 
