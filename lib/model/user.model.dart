e46bd1f6b3fd0bdc112c728d9981af5c906c0b32
Climpse <climpse.project@outlook.com>
2023-03-11 01:14:33 -0300

    feat: modelagem classe User

---
+++ lib/model/user.model.dart
@@ -0,0 +1,10 @@
+class User {
+  String? nome;
+  String? cpf;
+  String? email;
+  String? senha;
+  String? sexo;
+  String? celular;
+
+  User({this.nome, this.cpf, this.email, this.senha, this.sexo, this.celular});
+}
