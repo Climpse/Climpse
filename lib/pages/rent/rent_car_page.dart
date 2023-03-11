a1200ce2187c5f81eb733918cc2c46502e424561
Climpse <climpse.project@outlook.com>
2023-03-11 20:51:36 -0300

    feat: implementando listagem de carros da api

--- lib/pages/rent/rent_car_page.dart
+++ lib/pages/rent/rent_car_page.dart
@@ -1,29 +1,46 @@
+import 'package:climpse/hooks/use_vehicle.hook.dart';
 import 'package:climpse/widgets/rent_car_item_card.dart';
 import 'package:flutter/material.dart';
 
-class RentCarPage extends StatelessWidget {
-  final String texto;
+class RentCarPage extends StatefulWidget {
+  const RentCarPage({super.key});
 
-  const RentCarPage(this.texto, {super.key});
+  @override
+  State<RentCarPage> createState() => _RentCarPageState();
+}
 
-  void non() {}
+class _RentCarPageState extends State<RentCarPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView(
-              children: const <Widget>[
-                RentCarItemCard(),
-                RentCarItemCard(),
-                RentCarItemCard(),
+    return FutureBuilder(
+      future: getAllAvailableVehicles(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasData) {
+          List<dynamic> vehicles = snapshot.data!;
+          return Scaffold(
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: vehicles.length,
+                      itemBuilder: (context, index) => RentCarItemCard(
+                          vehicles[index]['nome'],
+                          vehicles[index]['marca'],
+                          vehicles[index]['valorDiaria'])),
+                ),
               ],
             ),
-          ),
-        ],
-      ),
+          );
+        }
+        return const Text('Carregando carros');
+      },
     );
   }
 }
